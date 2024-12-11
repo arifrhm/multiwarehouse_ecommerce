4e8178e6689416e17ebcce471833528d1521a37c
Arif Rahman <arif.rahman.rhm@gmail.com>
2024-12-11 22:06:13 +0700

    Initial commit boilerplate

---
+++ src/main/java/com/arif/multiwarehouse/MultiwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.arif.multiwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiwarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arif/multiwarehouse/MultiwarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arif.multiwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiwarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
